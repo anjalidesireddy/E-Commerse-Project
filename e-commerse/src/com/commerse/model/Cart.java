6353efb51d83a400b890696ab2c98f0304d54f1c
Anjali <anjalidesireddy@gmail.com>
2017-12-18 12:18:18 +0530

    Pojo's
    User, Product & Cart modal classes are defined

---
+++ e-commerse/src/com/commerse/model/Cart.java
@@ -0,0 +1,27 @@
+package com.commerse.model;
+public class Cart {
+	
+	private String userId;
+	private String ProductId;
+	private int quantity;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getProductId() {
+		return ProductId;
+	}
+	public void setProductId(String productId) {
+		ProductId = productId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+}
